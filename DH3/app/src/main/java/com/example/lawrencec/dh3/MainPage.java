fdaf79f642febc13a72c06e9fa9e694c7f858102
leej1996 <leej1996@gmail.com>
2017-01-28 23:30:30 -0500

    Added second page

--- DH3/app/src/main/java/com/example/lawrencec/dh3/MainPage.java
+++ DH3/app/src/main/java/com/example/lawrencec/dh3/MainPage.java
@@ -1,14 +1,19 @@
 package com.example.lawrencec.dh3;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.Spinner;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainPage extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
+    public final static String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,7 +21,7 @@ public class MainPage extends AppCompatActivity implements AdapterView.OnItemSel
 
         Spinner spinner = (Spinner) findViewById(R.id.spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.planets_array, android.R.layout.simple_spinner_item);
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.food_options, android.R.layout.simple_spinner_item);
         // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         // Apply the adapter to the spinner
@@ -31,5 +36,10 @@ public class MainPage extends AppCompatActivity implements AdapterView.OnItemSel
     @Override
     public void onNothingSelected(AdapterView<?> parent) {
 
+    }
+
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
     }
 }
